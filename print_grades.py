5e2464ed3b121decb428b00f3bb70b82932090c0
Nicole Prindle <nprindle18@gmail.com>
2021-02-27 01:17:51 -0500

    Make print_grades.py report errors
    Fixes the longstanding "part-grades" KeyError bug. Should now properly
    report error messages from LC-3 graders.

--- print_grades.py
+++ print_grades.py
@@ -5,41 +5,44 @@ import fractions
 import unidecode
 
 def open_config():
-	with open("zucchini.yml", 'r') as stream:
-	    try:
-	        return(yaml.load(stream))
-	    except yaml.YAMLError as exc:
-	        print(exc)
+    with open("zucchini.yml", 'r') as stream:
+        try:
+            return(yaml.load(stream, Loader=yaml.FullLoader))
+        except yaml.YAMLError as exc:
+            print(exc)
 
 def print_passed(name):
-	print('\nTEST: %-45s %-15s\n' % (name, "PASSED"))
+    print('\nTEST: %-45s %-15s\n' % (name, "PASSED"))
 
 def print_failed(name, reason):
-	print('\nTEST: %-45s %-15s\n' % (name, "FAILED"))
-	print('\n%20s\n' % (reason))
+    print('\nTEST: %-45s %-15s\n' % (name, "FAILED"))
+    print('\n%20s\n' % (reason))
 
 def main():
 
-	zucc_config = open_config()
+    zucc_config = open_config()
 
-	components = json.load(sys.stdin)
+    components = json.load(sys.stdin)
 
-
-	for i in range(len(components)):
-		component = components[i]
-		zucc_component = zucc_config["components"][i]
-
-
-		for j in range(len(component["part-grades"])):
-			graded_part = component["part-grades"][j]
-			zucc_part = zucc_component["parts"][j]
-
-			score = graded_part["score"]
-			grade = float(sum(fractions.Fraction(x) for x in unidecode.unidecode(score).split()))
-			if (grade == 1.0):
-				print_passed(zucc_part["test"])
-			else:
-				print_failed(zucc_part["test"],graded_part["log"])
+    for component, zucc_component in zip(components, zucc_config["components"]):
+        if "part-grades" not in component:
+            errors = []
+            if "error" in component:
+                errors.append(component["error"])
+            if "error-verbose" in component:
+                errors.append(component["error-verbose"])
+            if not errors:
+                errors = ["grader command failed"]
+            for zucc_part in zucc_component["parts"]:
+                print_failed(zucc_part["test"], '\n'.join(errors))
+        else:
+            for graded_part, zucc_part in zip(component["part-grades"], zucc_component["parts"]):
+                score = graded_part["score"]
+                grade = float(sum(fractions.Fraction(x) for x in unidecode.unidecode(score).split()))
+                if (grade == 1.0):
+                    print_passed(zucc_part["test"])
+                else:
+                    print_failed(zucc_part["test"], graded_part["log"])
 
 
 main()
