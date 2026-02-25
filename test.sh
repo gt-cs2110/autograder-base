b098915139cf33ee708d24b230dd048fbca64242
Henry Bui <henry.h.bui05@gmail.com>
2026-02-24 19:44:20 -0500

    Add github workflow + test

---
+++ test.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+# Create temporary directory
+ROOT_DIR=$(mktemp -d)
+SOURCE_DIR="$ROOT_DIR/source"
+RESULTS_DIR="$ROOT_DIR/results"
+
+mkdir "$SOURCE_DIR"
+mkdir "$RESULTS_DIR"
+
+# Upload a basic test harness into it
+cat > $SOURCE_DIR/zucchini.toml << EOF
+name = "Test"
+
+[[components]]
+name = "Main"
+weight = 100
+backend = { kind = "MultiCommandGrader" }
+parts = [
+    { summary = "Passes", command = "true", weight = 1 }
+]
+EOF
+mkdir $SOURCE_DIR/grading-files
+
+# Check it all runs!
+docker run --rm -v $SOURCE_DIR:/autograder/source -v $RESULTS_DIR:/autograder/results --entrypoint /autograder/run_autograder $TEST_TAG \
+    && [ "$(jq '.score' $RESULTS_DIR/results.json)" = 100.0 ]
+
+RESULT=$?
+
+if [ ! -z "$ROOT_DIR" ]; then
+    rm -rf "$ROOT_DIR"
+fi
+
+exit $RESULT
